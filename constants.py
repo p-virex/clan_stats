48dbe926a8427192e9fc0ece1d263fdc6976b690
p_virex <dawudoff@yandex.ru>
2020-02-25 17:19:12 +0300

    add URL

---
+++ constants.py
@@ -0,0 +1,13 @@
+"""
+Все запросы в URI-формате имеют следующий вид:
+
+http(s)://<server>/<API_name>/<method block>/<method name>/?<get params>Где:
+
+<server> — URI игрового сервера на соответствующем кластере
+<API_name> — версия API
+<method block> — название группы методов
+<method name> — название метода
+<get params> — параметры метода GET для запроса
+"""
+URL = 'https://api.worldoftanks.ru/wot'
+APPLICATION_ID = 'ae66c7931b72dfb5b148eb8960f83b28'
